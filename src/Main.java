5d341cfc81698942626a938a070bbf9c6934bee0
sakoichi <kouichi19980817@gmail.com>
2026-01-14 23:35:12 +0900

    feat: users can input task on CLI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 //javac -d bin src/*.java   srcフォルダ配下のjavaファイルを全てコンパイルして、binフォルダへclassファイルを配置
 //java -cp bin Main         実行ファイルはbinフォルダにあるよ～
@@ -7,20 +8,38 @@ public class Main {
     public static void main(String args[]){
         //タスクを保存するリスト
         List<Task> toDoList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int nextId = 1;
 
-        //タスクを追加してみる
-        toDoList.add(new Task(1, "Javaの勉強をする"));
-        toDoList.add(new Task(2, "Githubにプッシュする"));
-        toDoList.add(new Task(3, "ミーティングに参加する"));
-        toDoList.add(new Task(4, "仕様書の修正をする"));
+        System.out.println("=== Java todo app ===");
 
-        //2番目のタスクを完了にしてみる
-        toDoList.get(1).markAsDone();
+        while (true){
+            System.out.println("\n--- メニュー ---");
+            System.out.println("1. タスクを追加");
+            System.out.println("2. タスクを表示");
+            System.out.println("3. 終了");
+            System.out.println("\n選択してください。 >");
 
-        //一覧を表示する
-        System.out.println("--- 今日のTODOリスト ---");
-        for (Task task : toDoList){
-            System.out.println(task);
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) {
+                System.out.println("タスクを入力");
+                String title = scanner.nextLine();
+                toDoList.add(new Task(nextId++, title));
+                System.out.println("追加しました！");
+            } else if (choice.equals("2")) {
+                System.out.println("\n---現在のタスク---");
+                for(Task task : toDoList){
+                    System.out.println(task);
+                }
+            } else if (choice.equals("3")) {
+                System.out.println("さようなら！");
+                break;  //whileの無限ループから抜ける
+            } else {
+                System.out.println("1～3で入力してくださいね。");
+            }
         }
+        scanner.close();
+
     }
 }
