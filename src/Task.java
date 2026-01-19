bf67d9e2465fe31fc2ca518955bbfca95839b6f3
sakoichi <kouichi19980817@gmail.com>
2026-01-19 23:46:37 +0900

    feat: update Task.java for Ball func

--- src/Task.java
+++ src/Task.java
@@ -8,6 +8,7 @@ public class Task {
     private String title;
     private boolean isDone;
     private LocalDate deadline;
+    private String ballOwner;
 
     //曜日付きの日付型を作るフォーマット用の定数
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd (E)", Locale.JAPANESE);
@@ -19,6 +20,7 @@ public class Task {
         this.title = title;
         this.isDone = false; //初期設定は未完了
         this.deadline = deadline;
+        this.ballOwner = "ME";
     }
 
     //タスクの状態を表示しやすくするメソッド
@@ -26,7 +28,7 @@ public class Task {
     @Override
     public String toString(){
         String status = isDone ? "[済]" : "[未]";  //三項演算子
-        return String.format("%d : %s %s (期限：%s)", id, status, title, deadline.format(FORMATTER)); //%d = digit(整数)、%s = string(文字列)
+        return String.format("ID.%d [%-8s] %s | %s (期限：%s)", id, ballOwner, status, title, deadline.format(FORMATTER)); //%d = digit(整数)、%s = string(文字列)
     }
 
     //完了フラグを切り替えるメソッド
@@ -34,10 +36,12 @@ public class Task {
         this.isDone = true;
     }
 
-    //getter
+    //getter & setter
     public int getId(){ return id; };
     public String getTitle(){ return title; }
     public boolean isDone(){ return isDone; }
     public LocalDate getDeadline(){ return deadline; }
+    public String getBallOwner(){ return ballOwner; }
+    public void setBallOwner(String ballOwner){ this.ballOwner = ballOwner; }
 
 }
